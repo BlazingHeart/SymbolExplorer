bb8b910f665982900d309912ebfd6bcb0844437c
redwyre <redwyre32@hotmail.com>
2013-02-20 19:40:32 +1000

    Moved object file parsing to a seperate file, reduced framework requirement

---
+++ SymbolExplorerLib/ObjectFile.cs
@@ -0,0 +1,52 @@
+﻿using SymbolExplorerLib.Native;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SymbolExplorerLib
+{
+    public class ObjectFile
+    {
+        public struct ImageSection
+        {
+            public IMAGE_SECTION_HEADER Header;
+            public byte[] RawData;
+        }
+
+        public IMAGE_FILE_HEADER objHeader { get; set; }
+
+        public byte[] OptionalHeader { get; set; }
+
+        public ImageSection[] Sections { get; set; }
+
+
+        public void FromStream(Stream stream)
+        {
+            objHeader = Utils.StreamToStructure<IMAGE_FILE_HEADER>(stream);
+
+            if (objHeader.SizeOfOptionalHeader != 0)
+            {
+                int size = objHeader.SizeOfOptionalHeader;
+                OptionalHeader = new byte[size];
+                stream.Read(OptionalHeader, 0, size);
+            }
+
+            Sections = new ImageSection[objHeader.NumberOfSections];
+
+            for (int i = 0; i < objHeader.NumberOfSections; ++i)
+            {
+                IMAGE_SECTION_HEADER sectionHeader = Utils.StreamToStructure<IMAGE_SECTION_HEADER>(stream);
+                Sections[i].Header = sectionHeader;
+            }
+
+            for (int i = 0; i < objHeader.NumberOfSections; ++i)
+            {
+                uint size = Sections[i].Header.SizeOfRawData;
+                Sections[i].RawData = new byte[size];
+                stream.Read(Sections[i].RawData, 0, (int)size);
+            }
+        }
+    }
+}
--- SymbolExplorerLib/ObjectFileMember.cs
+++ SymbolExplorerLib/ObjectFileMember.cs
@@ -9,17 +9,7 @@ namespace SymbolExplorerLib
 {
     public class ObjectFileMember : ArchiveMember
     {
-        public struct ImageSection
-        {
-            public IMAGE_SECTION_HEADER Header;
-            public byte[] RawData;
-        }
-
-        public IMAGE_FILE_HEADER objHeader { get; set; }
-
-        public byte[] OptionalHeader { get; set; }
-
-        public ImageSection[] Sections { get; set; }
+        ObjectFile _objectFile = new ObjectFile();
 
         public override void FromStream(Stream stream)
         {
@@ -29,31 +19,7 @@ namespace SymbolExplorerLib
 
             long streamPostHeader = stream.Position;
 
-            long streamEndOffset = streamPostHeader + Header.Size;
-            
-            objHeader = Utils.StreamToStructure<IMAGE_FILE_HEADER>(stream);
-
-            if (objHeader.SizeOfOptionalHeader != 0)
-            {
-                int size = objHeader.SizeOfOptionalHeader;
-                OptionalHeader = new byte[size];
-                stream.Read(OptionalHeader, 0, size);
-            }
-
-            Sections = new ImageSection[objHeader.NumberOfSections];
-
-            for (int i = 0; i < objHeader.NumberOfSections; ++i)
-            {
-                IMAGE_SECTION_HEADER sectionHeader = Utils.StreamToStructure<IMAGE_SECTION_HEADER>(stream);
-                Sections[i].Header = sectionHeader;
-            }
-
-            for (int i = 0; i < objHeader.NumberOfSections; ++i)
-            {
-                uint size = Sections[i].Header.SizeOfRawData;
-                Sections[i].RawData = new byte[size];
-                stream.Read(Sections[i].RawData, 0, (int)size);
-            }
+            _objectFile.FromStream(stream);
 
             long offset = (streamPostHeader + Header.Size + 1) & ~0x1;
 

dc971c657052f6a263ceacc062234b8519fbe929
Simon Stevenson <redwyre32@hotmail.com>
2013-03-12 23:11:46 +1000

    updates to build script, added upload to s3

--- scripts/upload_to_s3.ps1
+++ scripts/upload_to_s3.ps1
@@ -1,12 +1,20 @@
-#requires –Version 3.0
+#xrxequires –Version 3.0
 Param([Parameter(Mandatory=$True)][string]$filePath, [string]$bucketPath = "\")
+$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-Set-StrictMode –Version 3.0
+Set-StrictMode –Version 2.0
 $ErrorPreference = "Stop"
 
-$root = Resolve-Path "${PSScriptRoot}\.."
+pushd
+cd "${PSScriptRoot}\AWSPowerShell\"
+Import-Module "AWSPowerShell.psd1"
+popd
 
-$ProjBinDir = Join-Path $root "SymbolExplorer\bin\Release"
+Set-DefaultAWSRegion us-east-1
+
+#$root = Resolve-Path "${PSScriptRoot}\.."
+
+#$ProjBinDir = Join-Path $root "SymbolExplorer\bin\Release"
 
 $creds = Get-AWSCredentials "symbolexplorer"
 
@@ -15,7 +23,5 @@ $fileName = (Split-Path $filePath -Leaf)
 $bucketKey = (Join-Path $bucketPath $FileName)
 
 echo "Attempting to upload ${filePath} to ${bucketKey}"
-
-Set-DefaultAWSRegion us-east-1
 
 Write-S3Object -BucketName "symbolexplorer" -Key $bucketKey -File $filePath -CannedACLName "PublicRead" -Credentials $creds

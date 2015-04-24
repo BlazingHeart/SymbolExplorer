e549a914881ca51c8318ffcfe5eccdca7c5a50f3
Simon Stevenson <redwyre@outlook.com>
2015-04-25 03:52:22 +1000

    Added ribbon to replace menu and toolbar

--- SymbolExplorer/MainWindow.xaml.cs
+++ SymbolExplorer/MainWindow.xaml.cs
@@ -16,13 +16,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using SymbolExplorer.Framework;
+using Fluent;
 
 namespace SymbolExplorer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RibbonWindow
     {
         public MainWindow()
         {
